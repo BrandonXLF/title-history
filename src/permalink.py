fa52c20b777602e3ab90e7eacfb6d87a3c2621d6
Brandon Fowler <brandfowl@gmail.com>
2023-04-02 05:07:52 -0400

    Crawl log to get entries
    Slower than using history

--- src/permalink.py
+++ src/permalink.py
@@ -8,8 +8,77 @@ from .projectinfo import get_project_info
 from .projectformatter import ProjectFormatter
 from .config import config
 
-def page_in_comment(page, comment):
-	return re.compile('(\[\[| |^)' + re.escape(page) + '(\]\]| |$)').search(comment)
+def get_log_param_sql(formatter: ProjectFormatter, page_ns, page_title):
+	page = formatter.format_title(page_ns, page_title)
+	
+	# Formats for log_param
+	#
+	# Very old Format:			[TITLE]
+	# Old format:				[TITLE]\n
+	# Old format (no redirect):	[TITLE]\n1
+	# New format:				...s:9:"4::target";s:[TITLE LENGTH]:"[TITLE]";...
+	return '(log_params = %s OR log_params = %s OR log_params = %s OR log_params LIKE %s)', (
+		page,
+		f"{page}\n",
+		f"{page}\n1",
+		f'%s:9:"4::target";s:{len(page)}:"{page}";%'
+	)
+
+def get_item(formatter: ProjectFormatter, move_entry):
+	try:
+		to_page = phpserialize.loads(move_entry[5])[b'4::target'].decode()
+	except:
+		to_page = move_entry[5].decode().split('\n', 1)[0]
+		
+	time = datetime.strptime(move_entry[0].decode(), '%Y%m%d%H%M%S')
+
+	return {
+		'type': 'move',
+		'from': formatter.format_title(move_entry[2], move_entry[3].decode()),
+		'to': to_page,
+		'time': f'{time:%#I:%M:%S %p, %#d %B %Y}' if os.name == 'nt' else f'{time:%-I:%M:%S %p, %-d %B %Y}',
+		'user': move_entry[1].decode(),
+		'comment': move_entry[4].decode() and formatter.format_comment(move_entry[4].decode()),
+		'id': str(move_entry[6])
+	}
+
+def crawl_entries(cursor, formatter: ProjectFormatter, starting_entry):
+	items = []
+	process_entry = starting_entry
+	
+	while process_entry:
+		items.append(get_item(formatter, process_entry))
+		
+		log_param_sql, log_param_params = get_log_param_sql(formatter, process_entry[2], process_entry[3].decode())
+		
+		cursor.execute(
+			f'''
+			SELECT
+				log_timestamp,
+				actor_name,
+				log_namespace,
+				log_title,
+				comment_text,
+				log_params,
+				log_id
+			FROM logging_logindex
+			LEFT JOIN comment_logging ON
+				log_comment_id = comment_id
+			LEFT JOIN actor_logging ON
+				log_actor = actor_id
+			WHERE
+				log_id < %s
+				AND log_type = 'move'
+				AND {log_param_sql}
+			ORDER BY log_id DESC
+			LIMIT 1
+			''',
+			(process_entry[6],) + log_param_params
+		)
+
+		process_entry = cursor.fetchone()
+
+	return items
 
 def permalink(project, page_id):
 	project_info = get_project_info(project)
@@ -39,95 +108,58 @@ def permalink(project, page_id):
 	if not page_info:
 		abort(404, f'Page with ID {page_id} does not exist.')
 	
-	page_name = formatter.format_title(page_info[0], page_info[1].decode())
+	page_ns = page_info[0]
+	page_title = page_info[1].decode()
+	log_param_sql, log_param_params = get_log_param_sql(formatter, page_ns, page_title)
 
-	# Considerations:
-	# - Compare against parent revision to make sure content hasn't changed
-	# - Log entry might have a larger timestamp than the revision since it's added after
+	# TODO: this ignores moves before deleted log entry
 	cursor.execute(
-		'''
+		f'''
 		SELECT
 			log_timestamp,
 			actor_name,
 			log_namespace,
 			log_title,
-			comment_revision.comment_text,
-			comment_logging.comment_text,
+			comment_text,
 			log_params,
 			log_id
-		FROM (
-			SELECT *
-			FROM revision_userindex
-			JOIN logging_userindex ON
-				log_type = 'move'
-				AND log_actor = rev_actor
-				AND log_timestamp = rev_timestamp
-			UNION SELECT *
-			FROM revision_userindex
-			JOIN logging_userindex ON
-				log_type = 'move'
-				AND log_actor = rev_actor
-				AND log_timestamp = CAST(CAST(rev_timestamp AS DATETIME) + 1 AS BINARY)
-		) AS rev_log
-		JOIN comment_revision ON
-			rev_log.rev_comment_id = comment_revision.comment_id
-		JOIN comment_logging ON
-			log_comment_id = comment_logging.comment_id
-		JOIN actor_logging ON
+		FROM logging_logindex
+		LEFT JOIN comment_logging ON
+			log_comment_id = comment_id
+		LEFT JOIN actor_logging ON
 			log_actor = actor_id
 		WHERE
-			rev_log.rev_page = %s
-			AND EXISTS (
+			log_type = 'move'
+			AND {log_param_sql}
+			AND NOT EXISTS(
 				SELECT *
-				FROM revision_userindex AS p_rev
+				FROM logging_logindex AS prev_log
 				WHERE
-					p_rev.rev_id = rev_log.rev_parent_id
-					AND p_rev.rev_sha1 = rev_log.rev_sha1
+					log_type = 'move'
+					AND prev_log.log_namespace = %s
+					AND prev_log.log_title = %s
+					AND prev_log.log_id > logging_logindex.log_id
 			)
-		ORDER BY log_timestamp DESC
+		ORDER BY log_id DESC
 		''',
-		(page_id,)
+		log_param_params + (page_ns, page_title)
 	)
 
 	items = []
-	prev_from = None
-
-	for move_entry in cursor.fetchall():
-		from_page = formatter.format_title(move_entry[2], move_entry[3].decode())
+	first = True
+	
+	for entry in cursor.fetchall():
+		if not first:
+			items[len(items) - 1]['gap'] = True
 		
-		try:
-			to_page = phpserialize.loads(move_entry[6])[b'4::target'].decode()
-		except:
-			to_page = move_entry[6].decode().partition('\n')[0]
-
-		rev_comment = move_entry[4].decode()
-
-		# Filter out talk pages and subpages moved at the same time
-		if not page_in_comment(from_page, rev_comment) or not page_in_comment(to_page, rev_comment):
-			continue
-
-		time = datetime.strptime(move_entry[0].decode(), '%Y%m%d%H%M%S')
-
-		items.append({
-			'type': 'move',
-			'from': from_page,
-			'to': to_page,
-			'time': f'{time:%#I:%M:%S %p, %#d %B %Y}' if os.name == 'nt' else f'{time:%-I:%M:%S %p, %-d %B %Y}',
-			'user': move_entry[1].decode(),
-			'comment': move_entry[5].decode() and formatter.format_comment(move_entry[5].decode()),
-			'id': str(move_entry[7])
-		})
-		
-		if prev_from and prev_from != to_page:
-			items[len(items) - 2]['gap'] = True
-		
-		prev_from = from_page
+		items += crawl_entries(cursor, formatter, entry)
+		first = False
 
 	return render_template(
 		'results.html',
 		items = items,
 		project = project,
 		project_url = project_url,
-		page = page_name,
+		page = formatter.format_title(page_ns, page_title),
 		page_id = page_id
 	)
