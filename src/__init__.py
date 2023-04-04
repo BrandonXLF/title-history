b98f34ea0dc39b3b5e00a48d1851102dec4003e1
Brandon Fowler <brandfowl@gmail.com>
2023-04-03 22:48:29 -0400

    Serve robots.txt from at /robots.txt

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 from werkzeug.exceptions import HTTPException
 from .getfrompage import get_from_page
 from .permalink import permalink
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('form.html')
+
+@app.route('/robots.txt')
+def robots_txt():
+	return send_from_directory(app.static_folder, request.path[1:])
 
 app.add_url_rule('/get', view_func = get_from_page)
 app.add_url_rule('/<project>/<page_id>', view_func = permalink)
